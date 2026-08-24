35197997ecbf3bdbfce3f82c4b5b856510b5079e
Marko Jovicic <jovicicmarkoj22@gmail.com>
2026-08-25 01:18:52 +0200

    chore: format code

--- src/middleware/authenticateMiddleware.ts
+++ src/middleware/authenticateMiddleware.ts
@@ -2,14 +2,13 @@ import type { Request, Response, NextFunction } from "express";
 import { UnauthorizedError } from "../errors/errorTypes.js";
 import { verify } from "../modules/auth/jwt.service.js";
 
-export function authenticate(req: Request, res: Response, next: NextFunction) {
+export function authenticate(req: Request, next: NextFunction) {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    throw new UnauthorizedError("Authentication required");
+  }
   try {
-    const authorization = req.headers.authorization;
-
-    if (!authorization) {
-      throw new UnauthorizedError("Authentication required");
-    }
-
     const [type, token] = authorization.split(" ");
 
     if (type !== "Bearer" || !token) {
